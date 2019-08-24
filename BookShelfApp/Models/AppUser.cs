cbf2eb66dbffdffabe147152a57e9e0ca2804a4c
Your Name <you@example.com>
2019-08-23 20:37:20 -0500

    made models

---
+++ BookShelfApp/Models/AppUser.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BookShelfApp.Models
+{
+    public class AppUser : IdentityUser
+    {
+        [Required]
+        [Display(Name = "First Name")]
+        public string FirstName { get; set; }
+
+        [Required]
+        [Display(Name = "Last Name")]
+        public string LastName { get; set; }
+
+        [Display(Name = "Full Name")]
+        public string FullName
+        {
+            get
+            {
+                return $"{FirstName} {LastName}";
+            }
+        }
+    }
+}
---
+++ BookShelfApp/Models/Book.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BookShelfApp.Models
+{
+    public class Book
+    {
+        [Required]
+        [Display(Name = "Book Id")]
+        public int Id { get; set; }
+
+        [Required]
+        [Display(Name = "ISBN")]
+        public string ISBN { get; set; }
+
+        [Required]
+        [Display(Name = "Book Title")]
+        public string Title { get; set; }
+
+        [Required]
+        [Display(Name = "Genre")]
+        public string Genre { get; set; }
+
+        [Required]
+        [Display(Name = "Date Published")]
+        public DateTime PublishDate { get; set; }
+
+        [Required]
+        [Display(Name = "Author")]
+        public string Author { get; set; }
+
+        [Required]
+        [Display(Name = "Book Owner")]
+        public string AppUser { get; set; }
+    }
+}
